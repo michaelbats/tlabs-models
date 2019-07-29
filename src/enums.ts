9211ab36dc690caa830a66306a0d2145d234d3ac
Sebastian Gug <sebastiangug@gmail.com>
2019-07-29 16:28:53 +0100

    added offices enum

--- src/enums.ts
+++ src/enums.ts
@@ -1,67 +1,74 @@
 export enum QAActions {
-	UrgentAttentionRequired = 'Urgent Attention Required',
-	RepairEncapsulate = 'Repair/Encapsulate',
-	ActionRequired = 'Action Required',
-	RemovalRecommended = 'Removal Recommended',
-	MaterialToBeManagedAsAsbestos = 'Material to be managed as asbestos',
-	NoAsbestosDetected = 'No asbestos detected',
-	NoSuspectMaterialIdnetified = 'No suspect material identified'
+    UrgentAttentionRequired = 'Urgent Attention Required',
+    RepairEncapsulate = 'Repair/Encapsulate',
+    ActionRequired = 'Action Required',
+    RemovalRecommended = 'Removal Recommended',
+    MaterialToBeManagedAsAsbestos = 'Material to be managed as asbestos',
+    NoAsbestosDetected = 'No asbestos detected',
+    NoSuspectMaterialIdnetified = 'No suspect material identified'
 }
 
 export enum QAIssueType {
-	LabIssues = 'Lab Issues',
-	SurveyorIssues = 'Surveyor Issues',
-	AdminIssues = 'Admin Issues',
-	Other = 'Other'
+    LabIssues = 'Lab Issues',
+    SurveyorIssues = 'Surveyor Issues',
+    AdminIssues = 'Admin Issues',
+    Other = 'Other'
 }
 
 export enum RaiseInvoice {
-	Manual = 'manual',
-	Automatic = 'automatic',
-	Bulk = 'bulk'
+    Manual = 'manual',
+    Automatic = 'automatic',
+    Bulk = 'bulk'
 }
 
 export enum AutomaticAnalysisReport {
-	Manual = 'manual',
-	Automatic = 'automatic'
+    Manual = 'manual',
+    Automatic = 'automatic'
 }
 
 export enum Likelihood {
-	CertainGuaranteed = 'Certain/Guaranteed',
-	Likely = 'Likely',
-	Possible = 'Possible',
-	Unlikely = 'Unlikely'
+    CertainGuaranteed = 'Certain/Guaranteed',
+    Likely = 'Likely',
+    Possible = 'Possible',
+    Unlikely = 'Unlikely'
 }
 
 export enum Status {
-	NotBooked = 'Not Booked',
-	Booked = 'Booked',
-	Finished = 'Finished'
+    NotBooked = 'Not Booked',
+    Booked = 'Booked',
+    Finished = 'Finished'
 }
 
 export enum Reason {
-	NoAnswer = 'No Answer',
-	CallRejectedByContact = 'Call Rejected By Contact',
-	WrongNumber = 'Wrong Number',
-	ContactUnavailable = 'Contact Unavailable',
-	CallbackRequested = 'Callback Requested'
+    NoAnswer = 'No Answer',
+    CallRejectedByContact = 'Call Rejected By Contact',
+    WrongNumber = 'Wrong Number',
+    ContactUnavailable = 'Contact Unavailable',
+    CallbackRequested = 'Callback Requested'
 }
 
 export enum RelatedCollection {
-	Clients = 'clients',
-	Contacts = 'contacts',
-	Jobs = 'jobs',
-	Leads = 'leads',
-	Personal = 'personal',
-	Projects = 'projects',
-	Quotes = 'quotes',
-	Sites = 'sites'
+    Clients = 'clients',
+    Contacts = 'contacts',
+    Jobs = 'jobs',
+    Leads = 'leads',
+    Personal = 'personal',
+    Projects = 'projects',
+    Quotes = 'quotes',
+    Sites = 'sites'
 }
 
 export enum ActivityType {
-	Email = 'email',
-	Note = 'note',
-	Call = 'call',
-	Update = 'update',
-	Meeting = 'meeting'
+    Email = 'email',
+    Note = 'note',
+    Call = 'call',
+    Update = 'update',
+    Meeting = 'meeting'
+}
+
+export enum Offices {
+    All = 'All',
+    Fen = 'Fenstanton',
+    Les = 'Leicester',
+    Lon = 'London'
 }
