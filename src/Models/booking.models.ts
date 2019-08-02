e23b3bf69c232e73b9cf396e6319fd189d27b461
sebastiangug <sebastian.gug@ishango.co.uk>
2019-08-02 15:53:28 +0100

    Booking model status BSON error

--- src/Models/booking.models.ts
+++ src/Models/booking.models.ts
@@ -2,75 +2,75 @@ import { prop, Typegoose, arrayProp } from 'typegoose';
 import { Without } from './shared.models';
 
 export class BookingSchema extends Typegoose {
-	@prop({ required: true })
-	_id?: string;
-	@prop()
-	jobId?: string;
-	@prop()
-	jobName?: string;
-	@prop()
-	batchId?: string;
-	@prop()
-	batchName?: string;
-	@prop()
-	projectId?: string;
-	@prop()
-	projectName?: string;
-	@prop()
-	clientId?: string;
-	@prop()
-	clientName?: string;
-	@prop()
-	teamsId?: string;
-	@prop()
-	engineerId?: string;
-	@prop()
-	notes?: string;
-	@prop()
-	status?: string;
-	@prop()
-	imageId?: string;
-	@prop()
-	confirmationCode?: string;
-	@prop()
-	confirmationCodeReason?: string;
-	@prop()
-	feedback?: string;
-	@prop()
-	isAVO?: boolean;
-	@prop()
-	startDate?: string;
-	@prop()
-	endDate?: string;
-	@prop()
-	isNotModifiable?: boolean;
-	@prop()
-	isLockedToEngineer?: boolean;
-	@prop()
-	actualStart?: string;
-	@prop()
-	actualEnd?: string;
-	@prop()
-	statusDate?: string;
-	@prop()
-	ended?: boolean;
-	@prop()
-	workCompleted?: boolean;
-	@prop({ required: true })
-	createdBy?: string;
-	@prop({ default: new Date(Date.now()) })
-	createdAt?: string;
-	@arrayProp({ items: String })
-	elementCompleted?: string[];
+  @prop({ required: true })
+  _id?: string;
+  @prop()
+  jobId?: string;
+  @prop()
+  jobName?: string;
+  @prop()
+  batchId?: string;
+  @prop()
+  batchName?: string;
+  @prop()
+  projectId?: string;
+  @prop()
+  projectName?: string;
+  @prop()
+  clientId?: string;
+  @prop()
+  clientName?: string;
+  @prop()
+  teamsId?: string;
+  @prop()
+  engineerId?: string;
+  @prop()
+  notes?: string;
+  @prop()
+  status?: string;
+  @prop()
+  imageId?: string;
+  @prop()
+  confirmationCode?: string;
+  @prop()
+  confirmationCodeReason?: string;
+  @prop()
+  feedback?: string;
+  @prop()
+  isAVO?: boolean;
+  @prop()
+  startDate?: string;
+  @prop()
+  endDate?: string;
+  @prop()
+  isNotModifiable?: boolean;
+  @prop()
+  isLockedToEngineer?: boolean;
+  @prop()
+  actualStart?: string;
+  @prop()
+  actualEnd?: string;
+  @prop({ required: true })
+  statusDate?: Date;
+  @prop()
+  ended?: boolean;
+  @prop()
+  workCompleted?: boolean;
+  @prop({ required: true })
+  createdBy?: string;
+  @prop({ default: new Date(Date.now()) })
+  createdAt?: string;
+  @arrayProp({ items: String })
+  elementCompleted?: string[];
 }
 
 export const Booking = new BookingSchema().getModelForClass(BookingSchema, {
-	schemaOptions: {
-		collection: 'bookings'
-	}
+  schemaOptions: {
+    collection: 'bookings'
+  }
 });
 
 export type IBooking = Without<
-	BookingSchema,
-	'getModelForClass' | 'setModelForClass' | 'buildSchema'
+  BookingSchema,
+  'getModelForClass' | 'setModelForClass' | 'buildSchema'
 >;
--- src/Models/shared.models.ts
+++ src/Models/shared.models.ts
@@ -4,181 +4,180 @@ import { QAActions, QAIssueType, RaiseInvoice, AutomaticAnalysisReport } from '.
 export type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
 
 export class GoogleFile {
-	@prop({ required: true })
-	fileId: string;
-	@prop({ required: true })
-	fileName: string;
-	@prop({ required: true })
-	fileUrl: string;
-	@prop({ required: true })
-	hideFromEngineer: boolean;
+  @prop({ required: true })
+  fileId: string;
+  @prop({ required: true })
+  fileName: string;
+  @prop({ required: true })
+  fileUrl: string;
+  @prop({ required: true })
+  hideFromEngineer: boolean;
 }
 
 export class GoogleFolder {
-	@prop({ required: true })
-	id: string;
-	@prop({ required: true })
-	name: string;
-	@prop({ required: true })
-	url: string;
+  @prop({ required: true })
+  id: string;
+  @prop({ required: true })
+  name: string;
+  @prop({ required: true })
+  url: string;
 }
 
 export class Element {
-	@prop({ required: true })
-	_id: string;
-	@prop({ required: true })
-	name: string;
-	@prop({ required: true })
-	description: string;
-	@prop({ default: 0 })
-	duration: number;
-	@prop({ default: 0 })
-	price: number;
-	@prop({ default: 0 })
-	priceWeekend: number;
-	@prop({ default: 0 })
-	reference: number;
+  @prop({ required: true })
+  _id: string;
+  @prop({ required: true })
+  name: string;
+  @prop({ required: true })
+  description: string;
+  @prop({ default: 0 })
+  duration: number;
+  @prop({ default: 0 })
+  price: number;
+  @prop({ default: 0 })
+  priceWeekend: number;
+  @prop({ default: 0 })
+  reference: number;
 }
 
 export class StepsType {
-	@prop()
-	action?: string;
-	@prop()
-	percentage?: number;
-	@prop()
-	delay?: number;
-	@prop()
-	associatedRole?: string;
+  @prop()
+  action?: string;
+  @prop()
+  percentage?: number;
+  @prop()
+  delay?: number;
+  @prop()
+  associatedRole?: string;
 }
 
 export class Steps {
-	@prop({ ref: StepsType, _id: false })
-	type: StepsType;
-	@prop()
-	label?: string;
+  @prop({ ref: StepsType, _id: false })
+  type: StepsType;
+  @prop()
+  label?: string;
 }
 
 export class WorkflowSteps {
-	@prop()
-	numberOfDays?: number;
-
-	@arrayProp({ itemsRef: Steps, _id: false })
-	steps: Steps[];
+  @prop()
+  numberOfDays?: number;
+  @arrayProp({ itemsRef: Steps, _id: false })
+  steps: Steps[];
 }
 
 export class Requirements {
-	@prop({ required: true })
-	reqId: string;
-	@prop({ required: true })
-	quantity: number;
-	@prop({ required: true })
-	clientCost: number;
-	@prop({ required: true })
-	externalCost: number;
+  @prop({ required: true })
+  reqId: string;
+  @prop({ required: true })
+  quantity: number;
+  @prop({ required: true })
+  clientCost: number;
+  @prop({ required: true })
+  externalCost: number;
 }
 
 export class Workflow {
-	@arrayProp({ itemsRef: WorkflowSteps, _id: false })
-	presite?: WorkflowSteps[];
-	@arrayProp({ itemsRef: WorkflowSteps, _id: false })
-	site?: WorkflowSteps[];
-	@arrayProp({ itemsRef: WorkflowSteps, _id: false })
-	postsite?: WorkflowSteps[];
+  @arrayProp({ itemsRef: WorkflowSteps, _id: false })
+  presite?: WorkflowSteps[];
+  @arrayProp({ itemsRef: WorkflowSteps, _id: false })
+  site?: WorkflowSteps[];
+  @arrayProp({ itemsRef: WorkflowSteps, _id: false })
+  postsite?: WorkflowSteps[];
 }
 
 export class ProjectContacts {
-	@prop()
-	contactId?: string;
-	@prop()
-	notes?: string;
-	@prop()
-	noAccess?: string;
-	@prop()
-	reports?: string;
-	@prop()
-	jobStatus?: string;
+  @prop()
+  contactId?: string;
+  @prop()
+  notes?: string;
+  @prop()
+  noAccess?: string;
+  @prop()
+  reports?: string;
+  @prop()
+  jobStatus?: string;
 }
 
 export class ProjectCompliance {
-	@prop()
-	hasMethodStatement?: boolean;
-	@prop()
-	hasScheduleOfRates?: boolean;
-	@prop({ enum: Object.values(AutomaticAnalysisReport) })
-	automaticAnalysisReport?: AutomaticAnalysisReport;
-	@prop({ enum: Object.values(RaiseInvoice) })
-	raiseInvoice?: RaiseInvoice;
+  @prop()
+  hasMethodStatement?: boolean;
+  @prop()
+  hasScheduleOfRates?: boolean;
+  @prop({ enum: Object.values(AutomaticAnalysisReport) })
+  automaticAnalysisReport?: AutomaticAnalysisReport;
+  @prop({ enum: Object.values(RaiseInvoice) })
+  raiseInvoice?: RaiseInvoice;
 }
 
 export class Team {
-	@arrayProp({ items: String })
-	admin?: string[];
-	@arrayProp({ items: String })
-	engineers?: string[];
-	@arrayProp({ items: String })
-	others?: string[];
+  @arrayProp({ items: String })
+  admin?: string[];
+  @arrayProp({ items: String })
+  engineers?: string[];
+  @arrayProp({ items: String })
+  others?: string[];
 }
 
 export class Samples {
-	@prop()
-	quantity?: number;
-	@prop()
-	trackingCode?: string;
-	@prop()
-	isUrgent?: boolean;
-	@prop()
-	isUrgentContact?: string;
-	@prop()
-	isUrgentReason?: string;
-	@prop()
-	analysisStartTime?: string;
-	@prop()
-	analysisEndTime?: string;
-	@prop()
-	qaStartTime?: string;
-	@prop()
-	qaEndTime?: string;
-	@prop({ enum: Object.values(QAActions) })
-	qaAction?: QAActions;
-	@prop()
-	qaIssues?: boolean;
-	@prop({ enum: Object.values(QAIssueType) })
-	qaIssueType: QAIssueType;
-	@prop()
-	teamsReport?: string;
+  @prop()
+  quantity?: number;
+  @prop()
+  trackingCode?: string;
+  @prop()
+  isUrgent?: boolean;
+  @prop()
+  isUrgentContact?: string;
+  @prop()
+  isUrgentReason?: string;
+  @prop()
+  analysisStartTime?: string;
+  @prop()
+  analysisEndTime?: string;
+  @prop()
+  qaStartTime?: string;
+  @prop()
+  qaEndTime?: string;
+  @prop({ enum: Object.values(QAActions) })
+  qaAction?: QAActions;
+  @prop()
+  qaIssues?: boolean;
+  @prop({ enum: Object.values(QAIssueType) })
+  qaIssueType: QAIssueType;
+  @prop()
+  teamsReport?: string;
 }
 
 export class ScheduleOfRates {
-	@prop({ required: true })
-	_id: string;
-	@prop({ required: true })
-	title: string;
-	@prop()
-	normalHours?: number;
-	@prop()
-	weekend?: number;
-	@prop()
-	duration?: number;
-	@prop()
-	internalProcedure?: string;
-	@prop()
-	description?: string;
-	@arrayProp({ items: String })
-	reqSkills?: string[];
-	@prop()
-	teamsAppointmentType?: string;
-	@prop()
-	teamsSurveyType?: string;
-	@prop()
-	teamsPropertyType?: string;
-	@prop()
-	teamsDistanceBand?: string;
-	@prop()
-	numberOfPumps?: number;
-	@prop()
-	teamsAirTestType?: string;
-	@arrayProp({ items: String })
-	elements?: string[];
-	@prop()
-	reference?: number;
+  @prop({ required: true })
+  _id: string;
+  @prop({ required: true })
+  title: string;
+  @prop()
+  normalHours?: number;
+  @prop()
+  weekend?: number;
+  @prop()
+  duration?: number;
+  @prop()
+  internalProcedure?: string;
+  @prop()
+  description?: string;
+  @arrayProp({ items: String })
+  reqSkills?: string[];
+  @prop()
+  teamsAppointmentType?: string;
+  @prop()
+  teamsSurveyType?: string;
+  @prop()
+  teamsPropertyType?: string;
+  @prop()
+  teamsDistanceBand?: string;
+  @prop()
+  numberOfPumps?: number;
+  @prop()
+  teamsAirTestType?: string;
+  @arrayProp({ items: String })
+  elements?: string[];
+  @prop()
+  reference?: number;
 }
