17c651c5139815e7ad23bc90ca09c0865e21e628
Michael Bats <michael@batstech.com>
2019-08-22 23:14:21 +0100

    removed ref bug

--- src/Models/external-user.model.ts
+++ src/Models/external-user.model.ts
@@ -1,79 +1,73 @@
-import { prop, Typegoose, Ref, arrayProp } from 'typegoose';
+import { prop, Typegoose } from 'typegoose';
 import { Without } from './shared.models';
 
 class Permissions {
-    @prop({ default: false })
-    reports: boolean;
+	@prop({ default: false })
+	reports: boolean;
 
-    @prop({ default: false })
-    orders: boolean;
+	@prop({ default: false })
+	orders: boolean;
 
-    @prop({ default: false })
-    activityLog: boolean;
+	@prop({ default: false })
+	activityLog: boolean;
 
-    @prop({ default: false })
-    admin: boolean;
+	@prop({ default: false })
+	admin: boolean;
 }
 
 class AuthMethods {
-    @prop({ default: false })
-    microsoft: boolean;
-    @prop({ default: false })
-    google: boolean;
-    @prop({ default: false })
-    local: boolean;
+	@prop({ default: false })
+	microsoft: boolean;
+	@prop({ default: false })
+	google: boolean;
+	@prop({ default: false })
+	local: boolean;
 }
 
 export class ExternalUserSchema extends Typegoose {
-    @prop({ required: true })
-    _id: string;
+	@prop({ required: true })
+	_id: string;
 
-    @prop({ default: null })
-    lastAuth: string;
+	@prop({ default: null })
+	lastAuth: string;
 
-    @prop({ ref: AuthMethods, _id: false })
-    authMethods: AuthMethods;
+	@prop()
+	authMethods: AuthMethods;
 
-    @prop({ required: true })
-    clientId: string;
+	@prop({ required: true })
+	clientId: string;
 
-    @prop({ required: true })
-    clientName: string;
+	@prop({ required: true })
+	clientName: string;
 
-    @prop({ unique: true, sparse: true })
-    googleId: string;
+	@prop({ unique: true, sparse: true })
+	googleId: string;
 
-    @prop({ unique: true, sparse: true })
-    microsoftId: string;
+	@prop({ unique: true, sparse: true })
+	microsoftId: string;
 
-    @prop({ required: true, unique: true })
-    email: string;
+	@prop({ required: true, unique: true })
+	email: string;
 
-    @prop({ required: true })
-    firstname: string;
+	@prop({ required: true })
+	firstname: string;
 
-    @prop({ required: true })
-    lastname: string;
+	@prop({ required: true })
+	lastname: string;
 
-    @prop({ default: Date.now() })
-    createdAt: Date;
+	@prop({ default: Date.now() })
+	createdAt: Date;
 
-    @prop({
-        ref: Permissions,
-        _id: false
-    })
-    permissions: Permissions;
+	@prop()
+	permissions: Permissions;
 }
 
-export const ExternalUser = new ExternalUserSchema().getModelForClass(
-    ExternalUserSchema,
-    {
-        schemaOptions: { collection: 'external_users' }
-    }
-);
+export const ExternalUser = new ExternalUserSchema().getModelForClass(ExternalUserSchema, {
+	schemaOptions: { collection: 'external_users' }
+});
 
 export type IExternalUser = Without<
-    ExternalUserSchema,
-    'getModelForClass' | 'setModelForClass' | 'buildSchema'
+	ExternalUserSchema,
+	'getModelForClass' | 'setModelForClass' | 'buildSchema'
 >;
 export type IPermissions = Permissions;
