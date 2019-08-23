02ac198ecef3e832a0bc9306a1c37f2eed3e3a69
Sebastian Gug <sebastiangug@gmail.com>
2019-08-23 13:08:45 +0100

    remodelled session

--- src/Models/session.model.ts
+++ src/Models/session.model.ts
@@ -1,30 +1,31 @@
 import { prop, Typegoose } from 'typegoose';
 import { Without } from './shared.models';
+import { IExternalUser } from 'src/models/external-user.model';
 export class SessionSchema extends Typegoose {
-	@prop({ required: true })
-	_id?: string;
+    @prop({ required: true })
+    _id?: string;
 
-	@prop({ required: true })
-	token: string;
+    @prop({ required: true })
+    agent: string;
 
-	@prop({ required: true })
-	details: string;
+    @prop({ required: true })
+    ip: string;
 
-	@prop({ required: true })
-	address: string;
+    @prop({ _id: false })
+    user: IExternalUser;
 
-	@prop({ default: new Date(Date.now()) })
-	createdAt?: string;
+    @prop({ default: new Date(Date.now()) })
+    createdAt?: string;
 
-	@prop({ default: new Date(Date.now()) })
-	lastAccess: string;
+    @prop({ default: new Date(Date.now()) })
+    lastAccess: string;
 }
 
 export const Session = new SessionSchema().getModelForClass(SessionSchema, {
-	schemaOptions: { collection: 'sessions' }
+    schemaOptions: { collection: 'sessions' }
 });
 
 export type ISession = Without<
-	SessionSchema,
-	'getModelForClass' | 'setModelForClass' | 'buildSchema'
+    SessionSchema,
+    'getModelForClass' | 'setModelForClass' | 'buildSchema'
 >;
